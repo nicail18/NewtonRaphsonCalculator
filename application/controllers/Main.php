027c16a5f4d951f0ff879a93f14c03132851e2bd
LAPTOP-4ONSEAPL\Nicail Pepito <nicail180@gmail.com>
2018-03-13 00:03:02 +0800

    Converted to Code Igniter

---
+++ application/controllers/Main.php
@@ -0,0 +1,34 @@
+<?php
+ defined('BASEPATH') OR exit('No direct script access allowed');
+ class Main extends CI_Controller {
+      //functions
+      public function index(){
+        $data["title"] = "Codeigniter Tutorial - Check Email availibility using Ajax";
+        $this->load->view("email_availability", $data);
+
+      }
+      function email_availibility(){
+          $data["title"] = "Codeigniter Tutorial - Check Email availibility using Ajax";
+          $this->load->view("email_availability", $data);
+      }
+      function check_email_avalibility()
+      {
+           if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL))
+           {
+                echo '<label class="text-danger"><span class="glyphicon glyphicon-remove"></span> Invalid Email</span></label>';
+           }
+           else
+           {
+                $this->load->model("main_model");
+                if($this->main_model->is_email_available($_POST["email"]))
+                {
+                     echo '<label class="text-danger"><span class="glyphicon glyphicon-remove"></span> Email Already register</label>';
+                }
+                else
+                {
+                     echo '<label class="text-success"><span class="glyphicon glyphicon-ok"></span> Email Available</label>';
+                }
+           }
+      }
+ }
+ ?>
---
+++ application/views/signin.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="<?php echo base_url('bootstrap/dist/css/bootstrap.min.css'); ?>" rel="stylesheet" />
+    <title>Sign In</title>
+</head>
+
+<body>
+  <div class="container">
+    <h1 align="center">Sign In</h1>
+    <a href="<?php echo base_url();?>Welcome/welcome_m">Click</a>
+  </div>
+</body>
+
+</html>
